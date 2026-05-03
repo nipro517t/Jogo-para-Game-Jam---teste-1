6b0f72198415965db75295a9e3d2fb0e6d66e877
Nicolas Camargo <nicolascrocha00@gmail.com>
2026-05-02 22:42:32 -0300

    melhorias

--- jonh-kagurabachi/scripts/heroi.gd
+++ jonh-kagurabachi/scripts/heroi.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -21,8 +21,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
